97f57f5d5f021923e40c8d8defbe141c95b7d710
imo3ath1 <iMo3ath28@gmail.com>
2024-04-19 15:46:41 +0300

    title1

--- add.php
+++ add.php
@@ -12,7 +12,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/box.css">
-    <title>Add post</title>
+    <title>Add post1</title>
 </head>
 <body>
     <?php include "header.php";?>
